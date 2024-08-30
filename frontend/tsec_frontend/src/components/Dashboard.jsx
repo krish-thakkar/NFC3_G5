ce0cd46063aedf00cba864460aebc200384e6eee
Maaz Saboowala <maazsaboowala07@gmail.com>
2024-08-30 07:39:15 +0530

    M

--- frontend/tsec_frontend/src/components/Dashboard.jsx
+++ frontend/tsec_frontend/src/components/Dashboard.jsx
@@ -192,7 +192,6 @@ const CropDashboard = () => {
           </Card>
         </div>
 
-<<<<<<< HEAD
         <div className="grid grid-cols-3 gap-6 mb-8">
           <Card info="Surface runoff is the flow of water that occurs on the ground surface when excess rainwater can no longer be absorbed by the soil.">
             <h2 className="text-xl font-semibold mb-4 text-green-800">Surface Runoff</h2>
@@ -222,37 +221,6 @@ const CropDashboard = () => {
             </div>
           </Card>
         </div>
-=======
-        <Card className="mb-8" theme={themeColors}>
-          <h2 className={`text-xl font-semibold mb-4 text-${themeColors.primary}-800`}>Crop Disease Identification</h2>
-          <p className="text-gray-700 mb-4">Upload an image of your crop to identify potential diseases and get treatment recommendations.</p>
-          <motion.button 
-            className={`bg-${themeColors.primary}-500 hover:bg-${themeColors.primary}-400 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:scale-105`}
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.95 }}
-          >
-            Upload Image
-          </motion.button>
-        </Card>
-        <Card className="mb-8" theme={themeColors}>
-          <h2 className={`text-xl font-semibold mb-4 text-${themeColors.primary}-800`}>Soil Type Classification</h2>
-          <p className="text-gray-700 mb-4">Upload an image</p>
-          <motion.button 
-            className={`bg-${themeColors.primary}-500 hover:bg-${themeColors.primary}-400 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out transform hover:scale-105`}
-            whileHover={{ scale: 1.05 }}
-            whileTap={{ scale: 0.95 }}
-          >
-            Upload Image
-          </motion.button>
-        </Card>
-      </div>
-      <button 
-        className={`fixed bottom-8 right-8 w-16 h-16 rounded-full bg-${themeColors.primary}-600 flex items-center justify-center shadow-lg text-white transition-transform transform hover:scale-105`} 
-        onClick={() => setShowChatbot(!showChatbot)}
-      >
-        
-      </button>
->>>>>>> 9aec4a832cb590f7d5a4a2c77c735ba8ebb4e15f
 
         <div className="mb-8">
           <Insights data={data} />
