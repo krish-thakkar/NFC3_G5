115a6fb6d48df475d1d512192c367ca9ed7f0fe5
Krish Thakkar <krishthakkar333@gmail.com>
2024-09-10 16:30:45 +0530

    removed api key

--- frontend/tsec_frontend/src/firebase.js
+++ frontend/tsec_frontend/src/firebase.js
@@ -2,11 +2,11 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDKFBQa0Ln8arcSiTICIA0Cn4gABYnNU0Q",
+  apiKey: "API_KEY",
   authDomain: "g5farmer.firebaseapp.com",
   projectId: "g5farmer",
   storageBucket: "g5farmer.appspot.com",
-  messagingSenderId: "1058874492736",
+  messagingSenderId: "SENDER_ID",
 };
 
 const app = initializeApp(firebaseConfig);
